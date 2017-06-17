e948a6ec91f3c8b5664a9ae17b7f7042c10fbeb9
Maxwell Benton <maxwellbenton@gmail.com>
2017-06-17 14:34:44 -0400

    changed controller

--- Mixd-in/app/controllers/api/drinks_controller.rb
+++ Mixd-in/app/controllers/api/drinks_controller.rb
@@ -1,7 +1,12 @@
 class Api::DrinksController < ApplicationController
   def index
     drinks = Drink.all
-
     render json: drinks
   end
+
+  def show
+    drink = Drink.find(params[:id])
+    render json: drink
+  end
+  
 end
