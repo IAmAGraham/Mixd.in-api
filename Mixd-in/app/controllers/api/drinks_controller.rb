ccbaee95da8515e07f1cdd96d0503ffc51d55506
Maxwell Benton <maxwellbenton@gmail.com>
2017-06-17 15:26:05 -0400

    fixed up create drink method

--- Mixd-in/app/controllers/api/drinks_controller.rb
+++ Mixd-in/app/controllers/api/drinks_controller.rb
@@ -13,6 +13,17 @@ class Api::DrinksController < ApplicationController
 
   def create
     drink = Drink.create(drinks_params)
+    
+    params['drink']['ingredients'].each {|ingre|
+      ing = Ingredient.find_or_create_by(name: ingre['name'], unit: ingre['unit'])
+      
+        amount = ingre['quantity'].to_i*2
+        amount.times do 
+          DrinkIngredient.create(drink_id: drink.id, ingredient_id: ing.id)
+        end
+      
+    }
+    
     render json: drink
   end
 
