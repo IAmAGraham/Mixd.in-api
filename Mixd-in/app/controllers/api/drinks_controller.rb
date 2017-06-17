5e12463fe454a0e9fbd8b4ba7738393d21e391e4
github username <github email address>
2017-06-17 14:31:05 -0400

    post request working exc for ingredients

--- Mixd-in/app/controllers/api/drinks_controller.rb
+++ Mixd-in/app/controllers/api/drinks_controller.rb
@@ -1,7 +1,18 @@
 class Api::DrinksController < ApplicationController
   def index
     drinks = Drink.all
-
     render json: drinks
+  end
+
+  def create
+    drink = Drink.create(drinks_params)
+    render json: drink
+  end
+
+
+  private
+
+  def drinks_params
+    params.require(:drink).permit(:name, :description, :ingredients)
   end
 end
