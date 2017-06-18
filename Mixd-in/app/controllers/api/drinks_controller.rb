201e1868f70b3ceb7ff9d47054b01e38032fab42
github username <github email address>
2017-06-18 10:52:11 -0400

    delete backend works

--- Mixd-in/app/controllers/api/drinks_controller.rb
+++ Mixd-in/app/controllers/api/drinks_controller.rb
@@ -9,22 +9,26 @@ class Api::DrinksController < ApplicationController
     drink = Drink.find(params[:id])
     render json: drink
   end
-  
+
 
   def create
     drink = Drink.create(drinks_params)
-    
+
     params['drink']['ingredients'].each {|ingre|
       ing = Ingredient.find_or_create_by(name: ingre['name'], unit: ingre['unit'])
-      
         amount = ingre['quantity'].to_i*2
-        amount.times do 
+        amount.times do
           DrinkIngredient.create(drink_id: drink.id, ingredient_id: ing.id)
         end
-      
-    }
-    
+      }
     render json: drink
+  end
+
+  def destroy
+    drink = Drink.find(params[:id])
+    drink.destroy
+    drinks = Drink.all
+    render json: drinks
   end
 
 
